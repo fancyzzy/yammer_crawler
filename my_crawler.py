09b89766c34501942d111a81ab80e9abcfa05c0e
fancyzzy <fancyzzy@163.com>
2018-08-06 15:23:46 +0800

    add my_crawler

---
+++ my_crawler.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+'''
+Crawl yammer message info
+by RESful API:
+https://www.yammer.com/api/v1/messages/in_group/15273590.json
+'''
+
+class My_Crawler():
+    def __init__(self):
+        pass
+
+    def download_all(self):
+        pass
+
+    def download(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    pass
