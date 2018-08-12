32509f28504dde0da703407a1dc1cbd0b34ac0a7
fancyzzy <fancyzzy@163.com>
2018-08-12 15:42:44 +0800

    uu

--- my_plot.py
+++ my_plot.py
@@ -7,39 +7,28 @@ Use matplotlib to draw  figures of  post and updates points
 
 import matplotlib.pyplot as plt
 import numpy as np
-from adjustText import adjust_text
 
 
+def draw_figure(data_list, threshold, date_end, date_start):
+    '''
 
-
-if __name__ == '__main__':
-
-    example_list = [[1639993032, u'LI Ellen', 141, 87, u'https://mug0.assets-yammer.com/mugshot/images/48x48/5h0BJ82WH7kQHlN3HJVbnrXTSMPBTfP1'],
-[1639849062, u'MAI Maggie', 39, 24, u'https://mug0.assets-yammer.com/mugshot/images/48x48/nbJkD-pZVvtD9WmNlgHzRxmsDj83glHw'],
-[1639869576, u'LI Xin', 36, 31, u'https://mug0.assets-yammer.com/mugshot/images/48x48/no_photo.png'],
-[1639997752, u'LI Hui', 36, 35, u'https://mug0.assets-yammer.com/mugshot/images/48x48/MqF14SXQ9mlm1wtKc80dZ32hJlKbdV0b'],
-[1540220095, u'ZHOU Ron', 15, 14, u'https://mug0.assets-yammer.com/mugshot/images/48x48/2Nq-KH4qCKjLjjlmhq7rvrdjRd2Z-FGN'],
-[1641702550, u'ZHANG Vicky', 12, 10, u'https://mug0.assets-yammer.com/mugshot/images/48x48/no_photo.png'],
-[1642625168, u'LIU Albert', 11, 6, u'https://mug0.assets-yammer.com/mugshot/images/48x48/SQcJk93P3S8VWH6TSXSBTX2mfzN8QzC4'],
-[1640337160, u'LIU Aaron', 10, 3, u'https://mug0.assets-yammer.com/mugshot/images/48x48/mbLFljlLhwkq2LttSJVn5C6TQpK01dnq'],
-[1576812066, u'LUO Turing', 9, 8, u'https://mug0.assets-yammer.com/mugshot/images/48x48/wbNBkQ3Z5qWPhQdJ9Tkwdb0WNpTrG98s'],
-[1639997404, u'YANG Ellen', 9, 6, u'https://mug0.assets-yammer.com/mugshot/images/48x48/dHF8k13FKzdj3H8cxtGCt1GrtGfNNB0n'],
-[1639911169, 'unknown user', 7, 1, 'https://mug0.assets-yammer.com/mugshot/images/48x48/no_photo.png'],
-[1610777031, u'XIE Crystal', 7, 4, u'https://mug0.assets-yammer.com/mugshot/images/48x48/hR45kTbX48WXnxJFK4JKDgXtKZ2hvRLq']]
-
-    threshold = 0
-    date_start = None
-    date_end = None
+    :param data_list: [[id,name,updates,posts,photo],...]
+    :param threshold:
+    :param date_end:
+    :param date_start:
+    :return:
+    '''
     if date_start == None:
         date_start = "beginning"
     if date_end == None:
         date_end = "Now"
-    post_list= [x[3] for x in example_list if x[2]>threshold or x[3]>threshold]
-    comment_list = [x[2] for x in example_list if x[2]>threshold or x[3]>threshold]
-    name_list = [x[1] for x in example_list if x[2]>threshold or x[3]>threshold]
-    photo_list= [x[-1] for x in example_list if x[2]>threshold or x[3]>threshold]
+    post_list= [x[3] for x in data_list if x[2]>threshold or x[3]>threshold]
+    comment_list = [x[2] for x in data_list if x[2]>threshold or x[3]>threshold]
+    name_list = [x[1] for x in data_list if x[2]>threshold or x[3]>threshold]
+    photo_list= [x[-1] for x in data_list if x[2]>threshold or x[3]>threshold]
 
     color = np.arctan2(comment_list, post_list)
+    #print("DEBUG color: {}".format(color))
 
     fig= plt.figure()
     ax1 = fig.add_subplot(111)
@@ -76,19 +65,35 @@ if __name__ == '__main__':
         old_x = post_list[i]
         old_y = comment_list[i]
         '''
+
         plt.annotate((name_list[i]+"\n(%d,%d)"%(post_list[i],comment_list[i])),\
         xy=(post_list[i], comment_list[i]), xytext=(post_list[i]+0.5, comment_list[i]+0.1),\
         fontsize=8)
 
-    texts = []
-    #xs = np.arange(post_list)
-    #ys = np.arange(comment_list)
-
-    '''
-    for x, y, s in zip(post_list, comment_list, labels):
-        texts.append(plt.text(x, y, s))
-    adjust_text(texts, only_move='y', arrowprops=dict(arrowstyle="->", color='r', lw=0.5))
-    '''
-
     plt.show()
+################draw_figure()##################################################################
+
+
+
+if __name__ == '__main__':
+
+    example_list = [[1639993032, u'LI Ellen', 141, 87, u'https://mug0.assets-yammer.com/mugshot/images/48x48/5h0BJ82WH7kQHlN3HJVbnrXTSMPBTfP1'],
+[1639849062, u'MAI Maggie', 39, 24, u'https://mug0.assets-yammer.com/mugshot/images/48x48/nbJkD-pZVvtD9WmNlgHzRxmsDj83glHw'],
+[1639869576, u'LI Xin', 36, 31, u'https://mug0.assets-yammer.com/mugshot/images/48x48/no_photo.png'],
+[1639997752, u'LI Hui', 36, 35, u'https://mug0.assets-yammer.com/mugshot/images/48x48/MqF14SXQ9mlm1wtKc80dZ32hJlKbdV0b'],
+[1540220095, u'ZHOU Ron', 15, 14, u'https://mug0.assets-yammer.com/mugshot/images/48x48/2Nq-KH4qCKjLjjlmhq7rvrdjRd2Z-FGN'],
+[1641702550, u'ZHANG Vicky', 12, 10, u'https://mug0.assets-yammer.com/mugshot/images/48x48/no_photo.png'],
+[1642625168, u'LIU Albert', 11, 6, u'https://mug0.assets-yammer.com/mugshot/images/48x48/SQcJk93P3S8VWH6TSXSBTX2mfzN8QzC4'],
+[1640337160, u'LIU Aaron', 10, 3, u'https://mug0.assets-yammer.com/mugshot/images/48x48/mbLFljlLhwkq2LttSJVn5C6TQpK01dnq'],
+[1576812066, u'LUO Turing', 9, 8, u'https://mug0.assets-yammer.com/mugshot/images/48x48/wbNBkQ3Z5qWPhQdJ9Tkwdb0WNpTrG98s'],
+[1639997404, u'YANG Ellen', 9, 6, u'https://mug0.assets-yammer.com/mugshot/images/48x48/dHF8k13FKzdj3H8cxtGCt1GrtGfNNB0n'],
+[1639911169, 'unknown user', 7, 1, 'https://mug0.assets-yammer.com/mugshot/images/48x48/no_photo.png'],
+[1610777031, u'XIE Crystal', 7, 4, u'https://mug0.assets-yammer.com/mugshot/images/48x48/hR45kTbX48WXnxJFK4JKDgXtKZ2hvRLq']]
+
+    threshold = 0
+    date_start = None
+    date_end = None
+
+    draw_figure(example_list, threshold, date_end, date_start)
+
     print("Done")
